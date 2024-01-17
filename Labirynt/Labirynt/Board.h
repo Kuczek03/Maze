54f67d6c97b5cd5d12c8c17406ffa27aed99506f
Kuczek03 <thekolorowyyt@gmail.com>
2024-01-18 00:37:23 +0100

    drawBoard

--- Labirynt/Labirynt/Board.h
+++ Labirynt/Labirynt/Board.h
@@ -1,15 +1,22 @@
 #include "asstes.h"
+#include <fcntl.h>
 class Board
 {
 private:
-	// Struktura reprezentuj¹ca wspó³rzêdne pola na planszy
+	// Structure representing coordinates on board
 	struct Point {
 		int x, y;
 
 		Point(int x, int y) : x(x), y(y) {}
 	};
+	vector<string> path;
+	queue<tuple<int, int, vector<string>>> queue; // BFS queue
+	
+	const int tileSize = 40;
 public:
+	vector<string> BFS(vector<vector<char>>& board);
+
 	bool isValid(int x, int y, int m, int n);
-	vector<string> findPath(vector<vector<char>>& board);
+	void drawBoard(sf::RenderWindow& window, const vector<vector<char>>& board, const vector<string>& path);
 };
 
