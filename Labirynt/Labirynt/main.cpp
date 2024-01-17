ab3317aae4739ef533157d6b3a3396cb4948ff35
Kuczek03 <thekolorowyyt@gmail.com>
2024-01-18 00:36:05 +0100

    SFML implementation

--- Labirynt/Labirynt/main.cpp
+++ Labirynt/Labirynt/main.cpp
@@ -1,51 +1,17 @@
 #include "asstes.h"
-#include "Board.h"
-#include "File.h"
-
+#include "Window.h"
+using namespace std;
 int main() {
-    setlocale(LC_CTYPE, "Polish");
-    int m, n;
-    string filename;
-    cout << "Podaj nazwê pliku z plansz¹: ";
+   
+    Window window;
+    
+    string filename, outname = "odp.txt";
+    cout << "Read board from file: ";
     cin >> filename;
-
-    Board brd;
-    File fl;
-    // Wczytaj planszê z pliku
-    vector<vector<char>> board = fl.loadBoardFromFile(filename, m, n);
-
-    // Wypisz planszê
-    cout << "Plansza:" << endl;
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    // Rozwi¹¿ problem labiryntu
-    vector<string> path = brd.findPath(board);
-
-    ofstream outputFile("odp.txt");
-    if (!outputFile.is_open()) {
-        cerr << "Nie mo¿na utworzyæ pliku odpowiedz.txt" << endl;
-        return EXIT_FAILURE;
-    }
-
-    if (path.empty()) {
-        outputFile << "nie" << endl;
-    }
-    else {
-        outputFile << "tak  ";
-        for (const string& direction : path) {
-            outputFile << direction << " ";
-        }
-        outputFile << endl;
-    }
-
-    outputFile.close();
-
-    cout << "Wynik zapisano w pliku odpowiedz.txt." << endl;
-
+    //cout << "Save in file: ";
+    //cin >> outname;
+   
+    window.windowLoop(filename, outname);
+   
     return 0;
 }
