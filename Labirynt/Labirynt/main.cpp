175fc206ee9d0698e16a3317998421b039f41b47
Kuczek03 <thekolorowyyt@gmail.com>
2024-01-18 01:31:35 +0100

    Added options to choose

--- Labirynt/Labirynt/main.cpp
+++ Labirynt/Labirynt/main.cpp
@@ -1,17 +1,46 @@
 #include "asstes.h"
+#include "Board.h"
 #include "Window.h"
 using namespace std;
-int main() {
-   
+int main() 
+{
     Window window;
-    
+    Board board;
+    int choice;
     string filename, outname = "odp.txt";
-    cout << "Read board from file: ";
-    cin >> filename;
-    //cout << "Save in file: ";
-    //cin >> outname;
+
+    cout << "1. Choose maze from the file. \n";
+    cout << "2. Choose random generated maze. \n";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+       
+        cout << "Read board from file: ";
+        cin >> filename;
+        //cout << "Save in file: ";
+        //cin >> outname;
+
+        window.mazeFromFile(filename, outname);
+        break;
+    case 2:
+        int m, n;
+        cout << "Podaj wymiary labiryntu (m n): ";
+        cin >> m >> n;
+
+        vector<vector<char>> maze = board.generateRandomMaze(m, n);
+
+        // Wypisz labirynt
+        cout << "Labirynt:" << endl;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cout << maze[i][j] << " ";
+            }
+            cout << endl;
+        }
+        break;
+    }
    
-    window.windowLoop(filename, outname);
    
     return 0;
 }
